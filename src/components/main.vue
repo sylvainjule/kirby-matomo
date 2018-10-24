e8a2cf85271e69dc1d90f737b70b480aaf48fcd5
Sylvain Julé <sylvain.jule@free.fr>
2018-10-24 21:38:11 +0200

    Don't require svg folder in main.vue

--- src/components/main.vue
+++ src/components/main.vue
@@ -28,8 +28,6 @@ import Chart from './widgets/chart.vue'
 import Overview from './widgets/overview.vue'
 import Widget from './widgets/widget.vue'
 
-import '../assets/svg/compiled'
-
 export default {
 	components: {Chart, Overview, Widget},
 	data() { 
