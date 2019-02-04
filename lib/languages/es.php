f72f389aaf5bd8196c9566851278c0938f5d83db
plantalabs <plantalabs@users.noreply.github.com>
2019-02-04 18:20:37 -0300

    spanish translation

---
+++ lib/languages/es.php
@@ -0,0 +1,79 @@
+<?php
+
+return array(
+
+	/* Titles
+	---------------------------*/
+
+	'matomo.title.external_link' => 'Tablero Matomo',
+	'matomo.title.overview'      => 'Visión general',
+	'matomo.title.details'       => 'Detalles',
+	'matomo.title.realtime'      => 'Ahora mismo',
+	'matomo.title.summary'       => 'Resumen de visitas',
+
+
+	/* Sublines
+	---------------------------*/
+
+	'matomo.subline.overview'       => '+/- basado en',
+	'matomo.subline.overview.day'   => 'Metricas de ayer.',
+	'matomo.subline.overview.week'  => 'Los últimos 7 días.',
+	'matomo.subline.overview.month' => 'El mes pasado, mismo periodo.',
+	'matomo.subline.overview.year'  => 'El año pasado, mismo periodo.',
+	'matomo.subline.details'        => 'Top {limit} – Ordenado por visitas.',
+
+
+	/* Periods
+	---------------------------*/
+
+	'matomo.chart.year'   => 'Este año',
+	'matomo.chart.month'  => 'Este més',
+	'matomo.chart.week'   => 'Últimos 7 días',
+	'matomo.chart.day'    => 'Hoy',
+
+	'matomo.period.year'  => 'Este año',
+	'matomo.period.month' => 'Este més',
+	'matomo.period.week'  => 'Esta semana',
+	'matomo.period.day'   => 'Hoy',
+
+
+	/* Widgets
+	---------------------------*/
+
+	'matomo.widgets.title.referrerType' => 'Tipo de referidos',
+	'matomo.widgets.title.websites'     => 'Sitios web de referidos',
+	'matomo.widgets.title.socials'      => 'Redes sociales',
+	'matomo.widgets.title.devicesType'  => 'Tipo de dispositivo',
+	'matomo.widgets.title.keywords'     => 'Palabras clave',
+	'matomo.widgets.title.popularPages' => 'Paginas populares',
+
+
+	/* Page widget
+	---------------------------*/
+
+	'matomo.widgets.page.metrics' => 'Metricas',
+	'matomo.widgets.page.year'    => 'del año',
+	'matomo.widgets.page.month'   => 'del més',
+	'matomo.widgets.page.week'    => 'de la semana',
+	'matomo.widgets.page.day'     => 'del día',
+
+
+	/* Misc. strings
+	---------------------------*/
+
+	'matomo.empty'            => 'No hay datos para mostrar',
+	'matomo.all'              => 'Todos',
+	'matomo.refresh'          => 'Recargar',
+	'matomo.visits'           => 'Visitas',
+	'matomo.duration'         => 'Duracion',
+	'matomo.duration.caption' => 'Es el promedio de duracion',
+	'matomo.bounce'           => 'Rebotes',
+	'matomo.bounce.caption'   => 'Visitantes que rebotaron (salieron del sitio despues de una sola pagina)',
+	'matomo.bounce_rate'      => 'Tasa de rebote',
+	'matomo.exit_rate'        => 'Tasa de salidas',
+	'matomo.actions'          => 'Accioness',
+	'matomo.actions.caption'  => 'por visita (páginas, descargas, enlaces y búsquedas internas del sitio)',
+	'matomo.visitors.caption' => ['visitante está navegando actualmente en el sitio web', 'visitantes están navegando actualmente en el sitio web'],
+	'matomo.novisitors'       => 'Actualmente no hay visitantes navegando por la web.',
+
+);
