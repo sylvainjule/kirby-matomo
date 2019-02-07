24782ad423e7508b5fa8b0c247c2b1c4edaaa5ba
Sylvain Julé <sylvain.jule@free.fr>
2019-02-07 09:16:31 +0100

    Update snippet

--- lib/snippets/matomo.php
+++ lib/snippets/matomo.php
@@ -1,16 +1,16 @@
 <?php if(Matomo::allowed()) : ?>
 
 	<script type="text/javascript">
-	    var _paq = _paq || [];
-	    _paq.push(['trackPageView']);
-	    _paq.push(['enableLinkTracking']);
+	  	var _paq = window._paq || [];
+		_paq.push(['trackPageView']);
+	  	_paq.push(['enableLinkTracking']);
 	    (function() {
 	    	var u='<?php echo option('sylvainjule.matomo.url'); ?>';
-	    	_paq.push(['setTrackerUrl', u+'piwik.php']);
-	    	_paq.push(['setSiteId', '<?php echo option('sylvainjule.matomo.id'); ?>']);
+	    	_paq.push(['setTrackerUrl', u+'matomo.php']);
+	    	_paq.push(['setSiteId', '1']);
 	    	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-	    	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-	    })();
+	    	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+	  	})();
 	</script>
 
 <?php else : ?>
