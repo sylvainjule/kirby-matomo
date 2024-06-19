b9f12b964e621aff3685f807818bb939210d9181
Sören <34400296+soerenengels@users.noreply.github.com>
2024-06-19 16:21:22 +0200

    fix deprecated k-inside

--- src/components/view.vue
+++ src/components/view.vue
@@ -1,5 +1,5 @@
 <template>
-    <k-inside>
+    <k-panel-inside>
         <k-view class="k-matomo">
             <k-header> {{ $t('matomo.title.overview') }} </k-header>
             <k-grid variant="columns">
@@ -18,7 +18,7 @@
                 </k-column>
             </k-grid>
         </k-view>
-    </k-inside>
+    </k-panel-inside>
 </template>
 
 <script>
